7d88494f54dc66c37767048ce061c01e610e2e7c
Daniel Rogowski <daniel.rogowski@onet.pl>
2024-03-26 15:48:05 +0100

    Initial commit

---
+++ backend/src/main/java/com/github/danrog303/poptracker/PopTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.github.danrog303.poptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopTrackerApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/github/danrog303/poptracker/PopTrackerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.github.danrog303.poptracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopTrackerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
